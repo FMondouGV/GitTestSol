7eee556e1f5ceedca1375f91e6b026917963ffc3
Fred M. <frederic.mondou@grassvalley.com>
2015-01-27 10:30:23 -0500

    Initial commit

---
+++ Git.Test.App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Test.App
+{
+    class Program
+    {
+        private static string test;
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void Method()
+        {
+            string v = inner.innerVar;
+        }
+
+        public class inner
+        {
+            public static string innerVar = Program.test;
+
+        }
+    }
+
+    class truc
+    {
+        public truc()
+        {
+            string v = Program.inner.innerVar;
+
+        }
+
+    }
+}
